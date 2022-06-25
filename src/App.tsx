8fb49d80ab461d9f571722256914c9bf1d12935e
Josué Heringer <josuedelgadoheringer98@gmail.com>
2022-06-24 22:50:41 -0300

    Finalizando terceira aula

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+import { client } from "./lib/apollo";
 import { Event } from "./pages/Event";
+import { Router } from "./Router";
 
 interface Lesson {
   id: string;
   title: string;
 }
 function App() {
-  return <Event />;
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  );
 }
 
 export default App;
